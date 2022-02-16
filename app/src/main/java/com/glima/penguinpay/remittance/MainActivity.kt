7390bb9482c1af82507cf6de8cc516438d6bef7d
Gustavo Lima <gustavosnl@gmail.com>
2022-02-15 23:52:34 -0300

    rearrange code

--- app/src/main/java/com/glima/penguinpay/remittance/MainActivity.kt
+++ app/src/main/java/com/glima/penguinpay/remittance/MainActivity.kt
@@ -21,15 +21,111 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         viewModel.updateExchangeTable()
+
         setupObservables()
         setupViews()
+    }
+
+    private fun setupObservables() {
+        observeAmount()
+        observeMoneyTransferSuccess()
+    }
+
+    private fun observeAmount() {
+        viewModel.convertedValue.observe(this) {
+            binding.textViewConvertedAmountValue.text =
+                resources.getString(R.string.exchanged_amount_value, it)
+        }
+    }
+
+    private fun observeMoneyTransferSuccess() {
+        viewModel.isMoneyTransferred.observe(this) { isSuccess ->
+            if (isSuccess) {
+                Snackbar.make(binding.root, R.string.money_transfer_succeed, Snackbar.LENGTH_SHORT)
+                    .show()
+            } else
+                Snackbar.make(binding.root, R.string.money_transfer_failed, Snackbar.LENGTH_SHORT)
+                    .show()
+        }
     }
 
     private fun setupViews() {
         setupAvailableMarkets()
         setupAmountInputEditText()
         setupFab()
+    }
+
+    private fun setupAvailableMarkets() {
+        adapter = MarketAdapter(this)
+        binding.spinner.adapter = adapter
+
+        populateAdapter()
+        setupOnItemSelect()
+    }
+
+    private fun populateAdapter() {
+        viewModel.receivingMarkets.observe(this) {
+            adapter.addAll(it)
+            adapter.notifyDataSetChanged()
+        }
+    }
+
+    private fun setupOnItemSelect() {
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(view: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
+                val market = (view?.getItemAtPosition(pos) as MarketVO)
+                changeCurrency(market.currency)
+                updatePhoneMask(market.digits)
+                updateCountryCode(market.countryCode)
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+            }
+        }
+    }
+
+    private fun setupAmountInputEditText() {
+        binding.editTextTextAmount.apply {
+            addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+                override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    viewModel.handleValueInput(text.toString(), getSelectedCurrency())
+                    updateConvertedLabelVisibility()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                }
+            })
+        }
+    }
+
+    private fun updateConvertedLabelVisibility() {
+        val visibility = if (getInputAmount().isNotEmpty()) View.VISIBLE else View.INVISIBLE
+        binding.textViewConvertedAmountLabel.visibility = visibility
+        binding.textViewConvertedAmountValue.visibility = visibility
+    }
+
+    private fun changeCurrency(currency: String) {
+        binding.textViewConvertedAmountLabel.text =
+            getString(R.string.exchanged_amount_label, currency)
+        viewModel.handleValueInput(
+            getInputAmount(),
+            currency
+        )
+    }
+
+    private fun updatePhoneMask(digits: Int) {
+        binding.editTextPhone.apply {
+            filters = arrayOf(InputFilter.LengthFilter(digits))
+            text.clear()
+        }
+    }
+
+    private fun updateCountryCode(countryCode: String) {
+        binding.textViewCountryCode.text = countryCode
     }
 
     private fun setupFab() {
@@ -57,102 +153,6 @@ class MainActivity : AppCompatActivity() {
         Snackbar.make(binding.root, R.string.missing_field_warning, Snackbar.LENGTH_SHORT).show()
     }
 
-    private fun updateConvertedLabelVisibility() {
-        val visibility = if (getInputAmount().isNotEmpty()) View.VISIBLE else View.INVISIBLE
-        binding.textViewConvertedAmountLabel.visibility = visibility
-        binding.textViewConvertedAmountValue.visibility = visibility
-    }
-
-    private fun setupAvailableMarkets() {
-        adapter = MarketAdapter(this)
-        binding.spinner.adapter = adapter
-
-        populateAdapter()
-        setupOnItemSelect()
-    }
-
-    private fun setupOnItemSelect() {
-        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(view: AdapterView<*>?, p1: View?, pos: Int, p3: Long) {
-                val market = (view?.getItemAtPosition(pos) as MarketVO)
-                changeCurrency(market.currency)
-                updatePhoneMask(market.digits)
-                updateCountryCode(market.countryCode)
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-        }
-    }
-
-    private fun updateCountryCode(countryCode: String) {
-        binding.textViewCountryCode.text = countryCode
-
-    }
-
-    private fun updatePhoneMask(digits: Int) {
-        binding.editTextPhone.apply {
-            filters = arrayOf(InputFilter.LengthFilter(digits))
-            text.clear()
-        }
-    }
-
-    private fun changeCurrency(currency: String) {
-        binding.textViewConvertedAmountLabel.text =
-            getString(R.string.exchanged_amount_label, currency)
-        viewModel.handleValueInput(
-            getInputAmount(),
-            currency
-        )
-    }
-
-    private fun getInputAmount() = binding.editTextTextAmount.text.toString()
-
-    private fun populateAdapter() {
-        viewModel.receivingMarkets.observe(this) {
-            adapter.addAll(it)
-            adapter.notifyDataSetChanged()
-        }
-    }
-
-    private fun setupObservables() {
-        observeAmount()
-        observeMoneyTransferSuccess()
-    }
-
-    private fun observeMoneyTransferSuccess() {
-        viewModel.isMoneyTransferred.observe(this) { isSuccess ->
-            if (isSuccess) {
-                Snackbar.make(binding.root, R.string.money_transfer_succeed, Snackbar.LENGTH_SHORT).show()
-            } else
-                Snackbar.make(binding.root, R.string.money_transfer_failed, Snackbar.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun observeAmount() {
-        viewModel.convertedValue.observe(this) {
-            binding.textViewConvertedAmountValue.text =
-                resources.getString(R.string.exchanged_amount_value, it)
-        }
-    }
-
-
-    private fun setupAmountInputEditText() {
-        binding.editTextTextAmount.apply {
-            addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-                override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    viewModel.handleValueInput(text.toString(), getSelectedCurrency())
-                    updateConvertedLabelVisibility()
-                }
-
-                override fun afterTextChanged(p0: Editable?) {
-                }
-            })
-        }
-    }
-
     private fun getSelectedMarket() = (binding.spinner.selectedItem as MarketVO)
 
     private fun getSelectedCurrency() = getSelectedMarket().currency
@@ -163,8 +163,5 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCompletePhone() = getSelectedCountryCode() + getPhone()
 
-
+    private fun getInputAmount() = binding.editTextTextAmount.text.toString()
 }
-
-
-
